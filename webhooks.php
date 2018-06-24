ff988bc165f0e4a5747cdf6cb771afc0241a244b
thatsayut <tenthat@gmail.com>
2018-06-24 21:54:21 +0700

    <commit message>

--- webhooks.php
+++ webhooks.php
@@ -16,9 +16,12 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+
+
+			$text = 'สวัสดีครับเป็นอย่างไรบ้าง';
 
 			// Build message to reply back
 			$messages = [
